56879787c7c62ff284c85b7df11429ab69b33153
Jason Huang <jason5528460@gmail.com>
2020-05-27 21:17:28 +0800

    modify class structure

--- mail.h
+++ mail.h
@@ -1,21 +1,36 @@
 #include <iostream>
 #include <string>
 #include <vector>
+using std::string;
+using std::vector;
+
+class MailBox;
+class Mail;
+
+class MailBox : public Mail {
+
+	public:
+		MailBox() : database(0); 
+		void add(string path);
+		void remove(int target_id);
+		void longest();
+	
+	private:
+		vector<Mail> database;
+};
 
 class Mail {
-	public:
-		Mail(std::string _from, std::string _to, std::string _date, int _id, std::string _subject) : from(_from), to(_to), date(_date), id(_id), subject(_subject);
-		add(std::string path);
-		remove(int target_id);
-		longest();
-		setContent(std::string readin); // split the original content into clean-words. (also set char_count)
 
-	private:
-		std::string from;
-		std::string to;
-		std::string date;
+	public:
+		Mail(string _from, string _to, string _date, int _id, string _subject) : from(_from), to(_to), date(_date), id(_id), subject(_subject);
+		void setContent(string readin); // split the original content into clean-words. (also set char_count)
+
+	protected:
+		string from;
+		string to;
+		string date;
+		string subject;
 		int id;
-		std::string subject;
-		std::vector<std::string> content;
 		int char_count;
+		vector<string> content;
 };
