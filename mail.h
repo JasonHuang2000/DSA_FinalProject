36b78920c4493819580a2a4bc11fe9d6f009d77d
Jason Huang <jason5528460@gmail.com>
2020-06-03 12:17:38 +0800

    add some STL data structure

--- mail.h
+++ mail.h
@@ -1,36 +1,63 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm> // make_heap(), push_heap(), pop_heap().
+#include <set> // std::multiset (rb-tree)
+#include <unordered_map> // std::unordered_multimap (hash-table)
+#include <functional>
 using std::string;
-using std::vector;
 
 class MailBox;
-class Mail;
+struct Mail;
+struct RbElem;
+struct HeapElem;
 
-class MailBox : public Mail { // storage for Mail, also has access to call the attributes of Mail.
+class MailBox { // storage for Mail.
 
 	public:
-		MailBox() : database(0); 
+		Mailbox() { std::make_heap(heap.begin(), heap.end()); }
 		void add(string path);
 		void remove(int target_id);
 		void longest();
 	
 	private:
-		vector<Mail> database;
+		std::unordered_multimap<string, Mail> hash_table;
+		std::multiset<RbElem, std::less<>> rb_tree;
+		std::vector<HeapElem> heap;
 };
 
-class Mail { // store the infomation of a mail.
-
-	public:
-		Mail(string _from, string _to, string _date, int _id, string _subject) : from(_from), to(_to), date(_date), id(_id), subject(_subject);
-		void setContent(string readin); // split the original content into clean-words. (also set char_count)
-
-	protected:
-		string from;
-		string to;
-		string date;
-		string subject;
-		int id;
-		int char_count;
-		vector<string> content;
+struct Mail { // store the infomation of a mail.
+	// variable
+	string from;
+	string to;
+	int date;
+	int id;
+	int char_count;
+	vector<string> content;
+	// function
+	Mail(string _from, string _to, int _date, int _id) : from(_from), to(_to), date(_date), id(_id);
+	void setContent(string readin); // split the original content into clean-words. (also set char_count)
 };
+
+struct RbElem { // Elements of Rb-tree(multiset).
+	// variable
+	int id;
+	int char_count;
+	string from;
+	int date;
+	// function
+	RbElem(int _id, int _count, string _from, int _date) : id(_id), char_count(_count), from(_from), date(_date);
+	bool operator < (const RbElem& rb) { return id < rb.id }
+	bool operator > (const RbElem& rb) { return id > rb.id }
+};
+
+struct HeapElem { // Elements of max-heap.
+	// variable
+	int id;
+	int char_count;
+	// function
+	HeapElem(int _id, int _count) : id(_id), char_count(_count);
+	bool operator < (const HeapElem& hp) { return char_count < hp.char_count}
+	bool operator > (const HeapElem& hp) { return char_count > hp.char_count}
+};
+
